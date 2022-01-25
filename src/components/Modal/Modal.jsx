cf6c419364d43c96b74fbb5a3bfb9f62fa6594f3
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-01-26 00:08:56 +0300

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { BsXCircle } from "react-icons/bs";
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
@@ -8,42 +8,35 @@ import { ModalBackdrop,
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    console.log('Modal ComponentDidMount');
+export const Modal = ({onClose, children}) => {
 
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    console.log('Modal ComponentWillUnmount');
-
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = event => {
+  const handleKeyDown = event => {
     if (event.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClickClose = event => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown, {
+      once: true
+    });
+  }, []);
+
+  const handleBackdropClickClose = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
     return createPortal(
-      <ModalBackdrop onClick={this.handleBackdropClickClose}>
-        <CloseBtn onClick={this.props.onClose}>
+      <ModalBackdrop onClick={handleBackdropClickClose}>
+        <CloseBtn onClick={onClose}>
           <BsXCircle size={40}/>
         </CloseBtn>
-        <ModalContent>{this.props.children}</ModalContent>
+        <ModalContent>{children}</ModalContent>
       </ModalBackdrop>,
       modalRoot,
     );
-  }
 };
 
 Modal.propTypes = {
